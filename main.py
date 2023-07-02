3ecbcf2d734a428fff14843a90075a0a917bfb65
sethbarryberg <bergseth98@gmail.com>
2023-07-02 14:57:53 -0700

    Initial commit Project2.a

---
+++ main.py
@@ -0,0 +1,11 @@
+print("please enter five numbers" )
+number1 = float(input())
+number2 = float(input())
+number3 = float(input())
+number4 = float(input())
+number5 = float(input())
+avg = (number1 + number2 + number3 + number4 + number5) / 5
+print("The average of your five numbers: ")
+print(avg)
+
+
